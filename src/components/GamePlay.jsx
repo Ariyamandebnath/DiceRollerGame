a2652af1d7746d10dd069fb03ff174e8cd9e0e42
Ariyaman Debnath <ariyamandebnath.ad@gmail.com>
2024-06-22 18:21:23 +0530

    NumberSelector Updated

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,55 +1,52 @@
-import React, { useState } from 'react'
+import  { useState } from 'react'
 import styled from 'styled-components';
-
+import TotalScore from './TotalScore';
 
 const GamePlay = () => {
-    const [score, setScore] = useState(0)
-    const [showRules, setShowRules] = useState(false)
+    
+const GamePlay = () => {
+    const [score, setScore] = useState(0);
+    const [selectedNumber, setSelectedNumber] = useState();
     const [currentDice, setCurrentDice] = useState(1);
-    const [selectNumber, setSelectNumber] = useState();
     const [error, setError] = useState("");
-     
+    const [showRules, setShowRules] = useState(false);
+
     const generateRandomNumber = (min, max) => {
         return Math.floor(Math.random() * (max - min) + min);
-    };
+  };
 
     const roleDice = () => {
-        if (!selectNumber) {
-            setError("Please select a number");
-            return
-        };
-
-        const randomNumber = generateRandomNumber(1, 7);
-        setCurrentDice((prev) => randomNumber);
-
-        if (selectNumber === randomNumber) {
-            setScore((prev) => prev + randomNumber);
-        } else {
-            setScore((prev)=> (prev <=0? 0: prev -2))
+        if (!selectedNumber) {
+        setError("You have not selected any number");
+        return;
         }
 
-        setSelectNumber(undefined);
-    }
-    
-    const resetScore = () => {
-        setScore(undefined);
+    const randomNumber = generateRandomNumber(1, 7);
+    setCurrentDice((prev) => randomNumber);
 
+    if (selectedNumber === randomNumber) {
+      setScore((prev) => prev + randomNumber);
+    } else {
+      setScore((prev) => prev - 2);
     }
 
+    setSelectedNumber(undefined);
+  };
+
+  const resetScore = () => {
+    setScore(0);
+  };
 
     return (
         <MainContainer>
             <div className="top_section">
-                
+                <TotalScore score={score} />
             </div>
-            
-    </MainContainer>
-    )
-}
-
+        </MainContainer>
+    );
+};
 
 export default GamePlay;
-
 
 const MainContainer = styled.main`
   padding-top: 70px;
@@ -65,6 +62,34 @@ const MainContainer = styled.main`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+  }
+`;
+
+
+    return (
+        <MainContainer>
+            <div className="top_section">
+                <TotalScore score={score} />
+            </div>
+        </MainContainer>
+    );
+};
+
+export default GamePlay;
+
+const MainContainer = styled.main`
+  padding-top: 70px;
+  .top_section {
+    display: flex;
+    justify-content: space-around;
+    align-items: end;
+  }
+  .btns {
+    margin-top: 40px;
     gap: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
   }
 `;
